0459a7a3749eb851bd59c102f16685a2b7164ab5
toma <toma.watanabe@rakus-partners.co.jp>
2022-11-14 10:39:31 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/items',
+        destination: 'http://localhost:8000/items',
+      },
+      {
+        source: '/api/items/:slug',
+        destination: 'http://localhost:8000/items/:slug',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
